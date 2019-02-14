77d4a059cf8ddcc2bf0c5f051c1697a27b042b10
Marianna Simões Bittencourt <41368195+MSBittencourt@users.noreply.github.com>
2019-02-14 13:21:05 +0100

    second

--- Node.JS-CLI/index.js
+++ Node.JS-CLI/index.js
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
-var validator = require("email-validator");
- 
-validator.validate("teste@mail.com"); // true
+const axios = require('axios');
+const validator = require("email-validator");
+const argv = require('yargs').argv;
 
-console.log("Hello, Node.JS!");
+if (validator.validate(argv._[0])){
+    const email = argv._[0];
+    const encodedEmail = encodeURIComponent(email);
+    const url = 
+        'https://haveibeenpwned.com/api/v2/breachedaccount/' + encodedEmail;
+
+axios
+.get(url, {headers: {'User-Agent': 'Node CLI tool'} })
+.then(function(response){console.log(response.data)})
+}
